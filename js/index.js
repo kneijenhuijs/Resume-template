37fdc36709cb30ff9aa071530926318cd49b11da
kneijenhuijs <k.neijenhuijs@gmail.com>
2017-09-13 11:07:15 +0200

    Created underline for active menu-text

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 var $divs = $("#pages > div");
 $("#menu-picker a").click(function() {
+	$('.menu-text').removeClass('currentlyActive');
+	$(this).addClass('currentlyActive');
 	$divs.hide().filter(".portfolio" + $(this).data('id')).show();
 }).first().click();
