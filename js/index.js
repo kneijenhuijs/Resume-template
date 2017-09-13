eadfbea61afb6a2e241b3c48f8cabe1a7a1d5795
kneijenhuijs <k.neijenhuijs@gmail.com>
2017-09-13 11:21:21 +0200

    Javascript fix
    Added document.ready

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
-var $divs = $("#pages > div");
-$("#menu-picker a").click(function() {
-	$('.menu-text').removeClass('currentlyActive');
-	$(this).addClass('currentlyActive');
-	$divs.hide().filter(".portfolio" + $(this).data('id')).show();
-}).first().click();
+$(document).ready(function(){
+	var $divs = $("#pages > div");
+	$("#menu-picker a").click(function() {
+		$('.menu-text').removeClass('currentlyActive');
+		$(this).addClass('currentlyActive');
+		$divs.hide().filter(".portfolio" + $(this).data('id')).show();
+	}).first().click();
+});
